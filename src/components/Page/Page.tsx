fdeea8b100123877874ece588d81d8c5d2741585
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-10-10 10:40:25 +0100

    Changed color mode button to use icons

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,25 +2,35 @@ import React from "react";
 import {
   Heading,
   Text,
-  HStack,
   VStack,
   Button,
   useColorMode,
+  Container,
+  Flex,
+  Spacer,
 } from "@chakra-ui/react";
+import { Icon } from "@iconify/react";
 
 const Page = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <div>
+    <Container maxW="5xl" p={4}>
       <VStack h="100vh" spacing={8} m={2}>
-        <HStack spacing={8}>
+        {/* Title and Color Mode Button */}
+        <Flex w="5xl" justify="space-between" p={4} gap="2">
           <Heading>Tic Tac Toe</Heading>
-          <Button onClick={toggleColorMode}>
-            Toggle {colorMode === "light" ? "Dark" : "Light"}
+          <Spacer />
+          <Button onClick={toggleColorMode} alignSelf="end">
+            <Icon
+              icon={colorMode === "light" ? "bi:moon-fill" : "bi:sun"}
+              inline={true}
+              height="1.4em"
+            />
           </Button>
-        </HStack>
+        </Flex>
 
+        {/* Game Board */}
         <Text>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas eius
           libero natus dolore eveniet maxime dolorem placeat sequi omnis atque
@@ -28,7 +38,7 @@ const Page = () => {
           quos?
         </Text>
       </VStack>
-    </div>
+    </Container>
   );
 };
 
