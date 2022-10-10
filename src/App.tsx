f032ed7a63c6abd0ebce0956c288efe62b712dd3
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-10-10 09:07:19 +0100

    added chakra-ui to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <ChakraProvider>
+      <Heading>Tic Tac Toe</Heading>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
