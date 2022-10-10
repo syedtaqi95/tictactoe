1a9ae89f865695edebd091d8fcd03d472c84b999
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-10-10 15:05:36 +0100

    added formatting to Square component

--- src/components/Board/Square.tsx
+++ src/components/Board/Square.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Box, GridItem } from "@chakra-ui/react";
+import { Box, GridItem, useColorModeValue } from "@chakra-ui/react";
 
 const Square = ({ row, col }: { row: Number; col: Number }) => {
+  const borderColor = useColorModeValue("black", "white");
   return (
-    <GridItem h="20" w="20" onClick={() => console.log(row, col)}>
-      <Box w="100%" h="100%" border="1px" borderColor="black" />
+    <GridItem
+      h="20"
+      w="20"
+      onClick={() => console.log(row, col)}
+      style={{ cursor: "pointer" }}
+    >
+      <Box w="100%" h="100%" border="1px" borderColor={borderColor} />
     </GridItem>
   );
 };
