12da1a4826925ea6520e237fd3d4d07999bd30b2
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-10-10 09:25:50 +0100

    Added chakra-ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Text, VStack } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <ChakraProvider>
-      <Heading>Tic Tac Toe</Heading>
+      <VStack h="100vh" spacing={8}>
+        <Heading>Tic Tac Toe</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas eius
+          libero natus dolore eveniet maxime dolorem placeat sequi omnis atque
+          unde necessitatibus aperiam error commodi, est, facilis laborum sint
+          quos?
+        </Text>
+      </VStack>
     </ChakraProvider>
   );
 };
